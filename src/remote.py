392d8941830bbe6e28e5d872edc46c7c188bcb45
Kalpan Jasani <jasani@adobe.com>
2021-12-21 09:29:16 -0800

    add support for multiple devices

--- src/remote.py
+++ src/remote.py
@@ -1,10 +1,16 @@
 """main runner for the project"""
 import sys
 import string
+import time
 
 import pychromecast
 from pychromecast.discovery import stop_discovery
 from .getch import getch
+
+TV_NAME = 'Living Room TV'
+
+# TODO: change
+SPEAKER_NAME = 'Kalpan\'s room speaker'
 
 # amount in seconds by which forward or backward buttons should move the song
 TIME_JUMP = 15
@@ -17,33 +23,48 @@ COMMANDS_MAP = {
     '-': 'decrease_volume',
     '7': 'back',
     '9': 'front',
+    '\t': 'switch_device'
 }
+
+
+def connect(device_name):
+    """connect to a device"""
+    chromecasts, browser = pychromecast.get_listed_chromecasts(
+        friendly_names=[device_name]
+    )
+
+    if not chromecasts:
+        print(f'{device_name} not discovered')
+        sys.exit(1)
+
+    cast = chromecasts[0]
+    print(f'Found {device_name}')
+
+    # Start socket client's worker thread and wait for initial status update
+    cast.wait()
+    browser.stop_discovery()
+
+    # nudge the volume up and down to show as connected
+    rc = cast.socket_client.receiver_controller
+    rc.update_status()
+    original_volume = rc.status.volume_level
+    rc.set_volume(original_volume-0.1)
+    time.sleep(1)
+    rc.set_volume(original_volume)
+
+    return cast
 
 
 def run():
     """run the remote"""
-    chromecasts, browser = pychromecast.get_listed_chromecasts(
-        friendly_names=["Living Room TV"]
-    )
-
-    if not chromecasts:
-        print('Chromecast not discovered')
-        sys.exit(1)
-
-    cast = chromecasts[0]
-    print('Found chromecast')
-    # Start socket client's worker thread and wait for initial status update
-    cast.wait()
-
-    mc = cast.media_controller
-    rc = cast.socket_client.receiver_controller
-
-    browser.stop_discovery()
+    cast = connect(TV_NAME)
+    mc, rc = cast.media_controller, cast.socket_client.receiver_controller
 
     # random initial assumption of whether subtitles are enabled. Used for
     # toggling between two states
     subtitles_enabled = True
-
+    tab_count = 0
+    active_device = TV_NAME
     # main read loop taking user input
     while(True):
         try:
@@ -57,6 +78,8 @@ def run():
                 break
             if ch == '\r':
                 print('pressed: \\r')
+            elif ch == '\t':
+                print('pressed: \\t')
             elif ch not in string.printable or ch in string.whitespace:
                 print('pressed: ?')
             else:
@@ -65,6 +88,23 @@ def run():
             mc.update_status()
             rc.update_status()
             command = COMMANDS_MAP.get(ch)
+
+            # a special case of a command to switch to the speaker to control
+            # it!
+            if command == 'switch_device':
+                tab_count += 1
+                if tab_count >= 2:
+                    print('switching devices')
+                    active_device = (
+                        SPEAKER_NAME if active_device == TV_NAME else TV_NAME)
+                    cast = connect(active_device)
+                    mc, rc = cast.media_controller, cast.socket_client.receiver_controller
+                    tab_count = 0
+
+            # resetting tab count as a double tap of tab is required to switch
+            else:
+                tab_count = 0
+
             if command == 'play':
                 mc.pause() if mc.status.player_is_playing else mc.play()
 
