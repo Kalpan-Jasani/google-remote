47028300947d3c849340c2276fc135cfe8a8f88e
Kalpan Jasani <jasani@adobe.com>
2021-12-21 16:52:26 -0800

    debug

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
+# note: to be only used inside the actualy pi acting as the remote.
 source /home/pi/.virtualenvs/google-remote/bin/activate
 cd /home/pi/projects/google-remote/
 python main.py
