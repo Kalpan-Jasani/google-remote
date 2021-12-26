988a39238565ab1fe0f55237d5e669248d88a139
Kalpan Jasani <jasani@adobe.com>
2021-12-25 23:59:03 -0800

    add a deploy file

---
+++ deploy/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ..
+
+# pull new changes
+git reset --hard HEAD
+git checkout raspberrypi
+git pull
+
+# place service file in system
+sudo cp google-remote.service /lib/systemd/system/google-remote.service
+
+# do things with service for systemd
+sudo systemctl daemon-reload
+sudo systemctl enable google-remote
+
+echo 'need to reboot to apply changes'
